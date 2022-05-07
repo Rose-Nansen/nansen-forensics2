b2f1414682271f1a24cb19efaaf54f3f722b8e20
rlmac <77149952+Rose-Nansen@users.noreply.github.com>
2022-05-07 12:20:41 +1000

    Update send.php

--- src/send.php
+++ src/send.php
@@ -1,11 +1,12 @@
 <?php
+echo "Got here";
 $name = $_POST['First name'] ['Last name'];
-$email = $_POST['email']
-$request = $_POST['form-control'] ['textarea'];
+$email = $_POST['email'];
+// $request = $_POST['form-control'] ['textarea'];
 
 $to = "rose@nansen.io";
 $subject = "Forensic Inquiry";
-$body = "Thank you for your message. We will reply to your inquiry soon. Please do not reply to this email. \n\n request";
+$body = $_POST['textarea'];
 
 mail ($to,$subject,$body);
 
